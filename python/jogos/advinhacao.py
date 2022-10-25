ff858ac11c0d706d7f12976866246708af1a2296
Victor Passos <vpassos10@hotmail.com>
2022-10-24 23:27:53 -0300

    python

---
+++ python/jogos/advinhacao.py
@@ -0,0 +1,19 @@
+import random
+
+print("--------------------------------")
+print("Bem vindo ao jogo de Adivinhação")
+print("--------------------------------")
+
+numero_secreto = random.randrange(1, 101)
+print(numero_secreto)
+
+chute = int(input("Digite o numero: "))
+
+print(f"Você digitou {chute}")
+
+if numero_secreto == chute:
+    print("Você acertou")
+else:
+    print("Você errou")
+
+print("--------------------------------\n", "Fim de Jogo!")
